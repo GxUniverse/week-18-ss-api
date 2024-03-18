5b7a8bb06a24ff2bc6b70989251daec00f135332
Manin Gauba <manindgauba@gmail.com>
2024-03-18 12:24:28 -0400

    added like and dislike functionality

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -43,9 +43,44 @@ postController.post('/:id', verifyToken, async(req, res) => {
     }
 })
 //delete a post
+postController.delete('/:id', verifyToken, async(req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
 
+        if(!post){
+            return res.status(500).json({msg:"No such post"})
+        } else if(post.userId !== req.user.id){
+            return res.status(403).json({msg: "Post is succesfully deleted"})
+        }
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+})
 
+//adding likes & dislikes
 
+postController.put('/likeDislike/:id', verifyToken, async(req,res) => {
+    try {
+        const currentUserId = req.user.id
+        const post = await Post.findById(req.params.id)
+
+        // if the user has already liked the post, remove it.
+        // Otherwise, add him into the likes array
+
+        if(post.likes.includes(currentUserId)){
+            post.likes = post.likes.filter((iod) => id!== currentUserId)
+            await post.save()
+            return res.status(200).json({msg:"Succesfully unliked the post"})
+        } else {
+            post.likes.push(currentUserId)
+            return res.status(200).json({msg: "Liked this idiots post"})
+        }
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+})
+
+module.exports = postController
 
 
 //edit a post
